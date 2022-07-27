87d3e0a43ec928af53961f2b5657c042b9ea92e6
arielsemik <47694409+arielsemik@users.noreply.github.com>
2022-07-27 10:18:53 +0200

    dodany allowed_host

--- KolazeZdjec/settings.py
+++ KolazeZdjec/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-@_8*&nafnv-ol_(by=vac75$*q5tk&km^6k!0%mz)t)lv#8(i4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['community.pythonanywhere.com']
 
 
 # Application definition
